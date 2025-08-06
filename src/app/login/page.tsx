ae1586b686ac1e2f0ee8040e790104a20e84ac6c
Aston Klingon <klingonaston@gmail.com>
2025-08-06 00:17:38 -0600

    Add Login Page Layout with Custom Background Image (#11)
    * Add login page component
    
    * Added background image for the login page
    
    ---------
    
    Co-authored-by: Felix Jordan <felixjordan.dev@gmail.com>

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,10 @@ import React from "react";
 
 function page() {
   return (
+    <div
+      className="min-h-screen bg-no-repeat bg-cover  flex flex-col-reverse items-center bg-purple-500 "
+      style={{ backgroundImage: "url('/images/login-bg.svg')" }}
+    >
     <div className="min-h-screen bg-no-repeat bg-cover  flex flex-col-reverse items-center bg-purple-500 ">
       <div className="bg-white bg-opacity-80 p-8 rounded shadow-md  mb-14">
         Login page
