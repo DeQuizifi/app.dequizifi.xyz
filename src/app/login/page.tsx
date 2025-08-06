1d605945e2228630a1053bc6ba967465fb3bea17
Aston Klingon <klingonaston@gmail.com>
2025-08-06 00:15:35 -0600

    Add login page component (#10)

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="min-h-screen bg-no-repeat bg-cover  flex flex-col-reverse items-center bg-purple-500 ">
+      <div className="bg-white bg-opacity-80 p-8 rounded shadow-md  mb-14">
+        Login page
+      </div>
+    </div>
+  );
+}
+
+export default page;
