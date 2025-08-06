20e7c42a004b70d517416f16ec096544b2f0b09d
Aston Klingon <klingonaston@gmail.com>
2025-08-06 00:00:24 -0600

    Added background image for the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,9 +6,10 @@ function page() {
       className="min-h-screen bg-no-repeat bg-cover  flex flex-col-reverse items-center bg-purple-500 "
       style={{ backgroundImage: "url('/images/login-bg.svg')" }}
     >
-    <div className="min-h-screen bg-no-repeat bg-cover  flex flex-col-reverse items-center bg-purple-500 ">
-      <div className="bg-white bg-opacity-80 p-8 rounded shadow-md  mb-14">
-        Login page
+      <div className="min-h-screen bg-no-repeat bg-cover  flex flex-col-reverse items-center bg-purple-500 ">
+        <div className="bg-white bg-opacity-80 p-8 rounded shadow-md  mb-14">
+          Login page
+        </div>
       </div>
     </div>
   );
