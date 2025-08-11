0a650d2443c30a08178f696d351c75146b7c323a
Aston Klingon <klingonaston@gmail.com>
2025-08-11 10:18:10 -0600

    Add TrendingQuiz component to dashboard with reusable card structure
    - Introduced a new TrendingQuiz component to display trending quizzes.
    - Created a ReusableCard component for consistent card design.
    - Updated the dashboard page to include the TrendingQuiz component.

---
+++ src/components/dashboard/TrendingQuiz.tsx
@@ -0,0 +1,54 @@
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import Image from "next/image";
+
+export default function TrendingQuiz() {
+  return (
+    <div className="border border-gray-200 bg-white rounded-xl p-4 shadow-sm">
+      <h1 className="text-2xl font-semibold font-mono mx-2 my-2">Trending Quizzes</h1>
+      <ReusableCard
+        title="DEX vs CEX"
+        description="20 questions"
+        action="1234"
+      />
+      <ReusableCard
+        title="Unstable Coin"
+        description="20 questions"
+        action="1204"
+      />
+    </div>
+  )
+}
+
+export function ReusableCard({ title, description, action }: { title: string; description: string; action: string }) {
+  return (
+    <Card className="my-2">
+      <CardHeader className="flex items-center justify-between">
+        <div className="flex items-start gap-3">
+          
+         <Image
+            src="/cube1.svg"
+            alt="cube"
+            width={48}
+            height={38}
+            className="rounded"
+          />
+          <div>
+            <CardTitle className="text-2xl font-mono">{title}</CardTitle>
+            <CardDescription className="font-mono">{description}</CardDescription>
+          </div>
+        </div>
+        <CardAction className="flex flex-col items-center font-mono font-semibold">
+          <span className="text-lg">{action}</span>
+          <span className="text-sm font-normal text-gray-500">Plays</span>
+        </CardAction>
+      </CardHeader>
+    </Card>
+  )
+}
+
