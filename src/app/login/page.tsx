6d661a883ea3ac025fd81b9ae8feb77a34e764d1
John Hagley <johnhagley.web3@gmail.com>
2025-08-17 23:31:21 -0600

    fix: adjust login page text position

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,14 +7,16 @@ function page() {
       style={{ backgroundImage: "url('/images/login-bg.svg')" }}
     >
       <div className="container mx-auto my-80 text-white p-8">
-        <h1 className="text-4xl font-bold mb-4">
-          Welcome <br /> to DequiziFi
-        </h1>
-        <p className="font-semibold mb-8">
-          Unlock Defi knowledge through fun, fast-
-          <br />
-          paced Quizzes
-        </p>
+        <div className="transform translate-y-32">
+          <h1 className="text-4xl font-bold mb-4">
+            Welcome <br /> to DeQuiziFi
+          </h1>
+          <p className=" mb-8">
+            Unlock Defi Knowledge Through Fun, Fast-
+            <br />
+            Paced Quizzes
+          </p>
+        </div>
         <WalletLoginButton />
       </div>
     </div>
