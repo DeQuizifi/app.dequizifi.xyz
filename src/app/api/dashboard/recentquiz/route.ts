e302e6cbe3e03bd51e52101a5e4989cd56f05f61
Olindo Task <olindost.sol@gmail.com>
2025-08-25 04:08:39 -0600

    feat: implement GET endpoint for recent quiz attempts based on wallet address

---
+++ src/app/api/dashboard/recentquiz/route.ts
@@ -0,0 +1,46 @@
+import prisma from "@/lib/prisma/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const wallet = searchParams.get("wallet");
+
+  if (!wallet) {
+    return NextResponse.json({ error: "Unauthorised" }, { status: 401 });
+  }
+  try {
+    const username = await prisma.user.findUnique({
+      where: {
+        walletAddress: wallet,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (!username) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+    const recentquiz = await prisma.quizAttempt.findFirst({
+      where: {
+        userId: username.id,
+      },
+      orderBy:{
+        createdAt: "desc",
+      },
+      select:{
+        quizId: true,
+        quiz:{
+            select:{
+                title:true,
+            }
+        }
+      }
+    });
+    return NextResponse.json({ recentquiz });
+  } catch {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
