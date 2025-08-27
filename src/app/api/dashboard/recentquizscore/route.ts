194c384e0d2c23042a994705bbbaf8d5106e4cac
Aston Klingon <klingonaston@gmail.com>
2025-08-26 22:08:42 -0600

    Add recent quiz score API endpoint for dashboard (#71)
    * feat: implement GET endpoint for recent quiz attempts based on wallet address
    
    * Add recent quiz score API endpoint for dashboard
    
    * Add recent quiz score API endpoint for dashboard
    
    * feat: implement JWT authentication for wallet login and recent quiz access
    
    * feat: validate JWT secret during module load to ensure proper configuration
    
    * Add recent quiz score API endpoint for dashboard
    
    * Add recent quiz score API endpoint for dashboard
    
    * refactor: replace wallet query parameter with JWT token validation for authorization
    
    ---------
    
    Co-authored-by: Olindo Task <olindost.sol@gmail.com>

---
+++ src/app/api/dashboard/recentquizscore/route.ts
@@ -0,0 +1,55 @@
+import { verifyToken } from "@/lib/auth";
+import prisma from "@/lib/prisma/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const authHeader = req.headers.get("authorization")
+    if(!authHeader?.startsWith("Bearer ")){
+      return NextResponse.json({error:"Unauthorized"},{status: 401})
+    }
+  
+    const token = authHeader.split(" ")[1];
+    const decoded = verifyToken(token);
+    if(!decoded){
+      return NextResponse.json({error:"Invalid or expired token"},{status:401})
+    }
+  
+    const wallet = decoded.wallet;
+  try {
+    const username = await prisma.user.findUnique({
+      where: {
+        walletAddress: wallet,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (!username) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+    const recentquiz = await prisma.quizAttempt.findFirst({
+      where: {
+        userId: username.id,
+      },
+      orderBy:{
+        createdAt: "desc",
+      },
+      select:{
+        quizId: true,
+        score:true,
+        quiz:{
+            select:{
+                title:true,
+            }
+        }
+      }
+    });
+
+    return NextResponse.json({ recentquiz });
+  } catch {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
