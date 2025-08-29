80ee57f67abca6527d044e7d681bc2007851719a
Kevin Ackerman <ackermankevin176@gmail.com>
2025-08-29 08:26:26 -0600

    feat: update latest added quizzes API to select specific fields and modify LatestTab component to display quiz title

--- src/app/api/search/latestaddedquizzes/route.ts
+++ src/app/api/search/latestaddedquizzes/route.ts
@@ -1,23 +1,30 @@
 import prisma from "@/lib/prisma/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(){
-    try{
-        const quizzes = await prisma.quiz.findMany({
-            orderBy:{
-                createdAt: "desc",
-            },
-            take: 6,
-            include:{
-                questions: true,
-                attempts: true,
-            }
-        });
-        if(quizzes.length === 0){
-            return NextResponse.json({error:"No quizzes found"},{status:404})
-        }
-        return NextResponse.json({quiz: quizzes},{status:200})
-        }catch(error){
-            return NextResponse.json({error:"Something went wrong"},{status:500})
+export async function GET() {
+  try {
+    const quizzes = await prisma.quiz.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: 6,
+      select: {
+        id: true,
+        title: true,
+        category: true,
+        createdAt: true,
+        questions: true,
+        attempts: true,
+      },
+    });
+    if (quizzes.length === 0) {
+      return NextResponse.json({ error: "No quizzes found" }, { status: 404 });
     }
+    return NextResponse.json({ quiz: quizzes }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
+  }
 }
