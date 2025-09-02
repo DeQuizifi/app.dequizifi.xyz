3571a4eb842126b9c202a40a325e408e0f4e6cad
Kevin Ackerman <ackermankevin176@gmail.com>
2025-09-01 22:38:38 -0600

    Add Trending Contest API: Returns Top 3 Contests with Participant Count and Time Left (#91)
    * feat: Add Contest and ContestParticipant models with seeder scripts for database initialization
    
    * feat: Add validation to contestParticipantSeed to skip seeding if no users or contests are available
    
    * feat: Implement GET endpoint for trending contests with summary data
    
    ---------
    
    Co-authored-by: John Hagley <johnhagley.web3@gmail.com>

---
+++ src/app/api/play/trendingContest/route.ts
@@ -0,0 +1,40 @@
+import prisma from "@/lib/prisma/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  try {
+    const trendingContest = await prisma.contest.findMany({
+      select: {
+        name: true,
+        participants: true,
+        startTime: true,
+      },
+      orderBy: {
+        startTime: "desc", // highest time to start first
+      },
+      take: 3,
+    });
+
+    const contestsSummary = trendingContest.map((contest) => {
+      const timeLeftMs = new Date(contest.startTime).getTime() - Date.now();
+      const timeLeftHours = Math.max(
+        0,
+        Math.round(timeLeftMs / (1000 * 60 * 60))
+      );
+      return {
+        name: contest.name,
+        participantCount: Array.isArray(contest.participants)
+          ? contest.participants.length
+          : 0,
+        timeLeftHours,
+      };
+    });
+    return NextResponse.json(contestsSummary);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
