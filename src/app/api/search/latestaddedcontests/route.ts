375564a6983dab6b85933afa825a59e76cb79fec
Lance Herman <lance.herman.btc@gmail.com>
2025-09-04 21:58:35 -0600

    Implement GET endpoint for latest added contests with time left to begin and participant count (#107)
    * feat: add registrationEndTimeHours to Contest model and update seeder logic
    
    * feat: implement GET endpoint for trending contests with error handling
    
    * feat: replace registrationEndTimeHours with registrationEndTime in Contest model and update related logic
    
    * feat: replace TrendingQuiz with TrendingContest component and implement TrendingContestList for dynamic contest data
    
    * fix: update message for empty trending contests to reflect correct terminology
    
    * feat: update LatestTab component to fetch quizzes and display with improved layout
    
    * feat: implement GET endpoint for latest added contests with time left and participant count
    
    ---------
    
    Co-authored-by: Olindo Task <olindost.sol@gmail.com>
    Co-authored-by: Aston Klingon <klingonaston@gmail.com>
    Co-authored-by: John Hagley <johnhagley.web3@gmail.com>
    Co-authored-by: Kevin Ackerman <ackermankevin176@gmail.com>

---
+++ src/app/api/search/latestaddedcontests/route.ts
@@ -0,0 +1,43 @@
+import prisma from "@/lib/prisma/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const latestContest = await prisma.contest.findMany({
+      select: {
+        name: true,
+        startTime: true,
+        createdAt: true,
+        _count: {
+          select: {
+            participants: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    if (latestContest.length == +0) {
+      return NextResponse.json({ error: "No Contests Found" }, { status: 404 });
+    }
+
+    // Calculate time left to begin in hours for each contest
+    const contestsWithTimeLeft = latestContest.map((contest) => {
+      const timeleftToBegin =
+        (new Date(contest.startTime).getTime() - Date.now()) / (1000 * 60 * 60); // hours
+      return {
+        ...contest,
+        timeleftToBegin,
+      };
+    });
+
+    return NextResponse.json(contestsWithTimeLeft, { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
