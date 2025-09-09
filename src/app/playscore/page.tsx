85e25ecc5a5f1da8ea5b3f3e4cb6f916ce22793f
Lance Herman <lance.herman.btc@gmail.com>
2025-09-09 09:56:45 -0600

    feat: Add ScoreCard component for displaying quiz scores and statistics

--- src/app/playscore/page.tsx
+++ src/app/playscore/page.tsx
@@ -1,9 +1,15 @@
 import ScoreHeader from "@/components/play/scorepage/header";
+import ScoreCard from "@/components/play/scorepage/scorecard";
 
 export default function PlayScore() {
   return (
-    <div>
-      <ScoreHeader />
+    <div className="w-full min-h-screen bg-background flex flex-col items-center justify-start pt-8">
+      <div className="relative w-full flex flex-col items-center">
+        <ScoreHeader />
+        <div className="absolute left-1/2 -translate-x-1/2 mt-70 z-20">
+          <ScoreCard />
+        </div>
+      </div>
     </div>
   );
 }
