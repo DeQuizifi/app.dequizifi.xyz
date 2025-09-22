661a19c2166a7fb8b81481d44a005b41568d4766
Olindo Task <olindost.sol@gmail.com>
2025-09-22 04:14:40 -0600

    chore: restart app.dequizifi project for major backend overhaul

--- prisma/migrations/20250829095602_add_progress_is_finished_to_quiz_attempt/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."QuizAttempt" ADD COLUMN     "isFinished" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "progress" DOUBLE PRECISION NOT NULL DEFAULT 0;
--- prisma/migrations/20250904114025_add_reg_end_time_datetime/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `registrationEndTimeHours` on the `Contest` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Contest" DROP COLUMN "registrationEndTimeHours",
-ADD COLUMN     "registrationEndTime" TIMESTAMP(3);
