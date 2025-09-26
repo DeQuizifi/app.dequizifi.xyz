6ff0802bbe60385237fc7d98398ba0c42278bd64
John Hagley <johnhagley.web3@gmail.com>
2025-09-25 21:46:31 -0600

    fix: format PlayPageContainer component for consistent spacing

--- src/app/(general)/play/page.tsx
+++ src/app/(general)/play/page.tsx
@@ -1,11 +1,10 @@
 import { GeneralLayout } from "@/components/layout/GeneralLayout";
 import { PlayPageContainer } from "@/components/pages/play/PlayPageContainer";
 
-
 export default function Play() {
   return (
     <GeneralLayout>
-        <PlayPageContainer/>
+      <PlayPageContainer />
     </GeneralLayout>
   );
 }
