16d083062c23ab9d2d048260041d21d2d4e3fc1f
Olindo Task <olindost.sol@gmail.com>
2025-10-01 10:44:14 +0000

    feat: replace Geist font with Josefin Sans and add keywords to metadata for improved SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Josefin_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+
+const josefinSans = Josefin_Sans({
+  variable: "--font-josefin-sans",
   subsets: ["latin"],
 });
 
@@ -15,6 +16,18 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "DeQuizifi",
   description: "The ultimate quiz platform",
+  keywords: [
+    "quiz",
+    "platform",
+    "trivia",
+    "questions",
+    "games",
+    "DeQuizifi",
+    "online quiz",
+    "knowledge",
+    "competition",
+    "education"
+  ],
 };
 
 export default function RootLayout({
@@ -23,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${josefinSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
